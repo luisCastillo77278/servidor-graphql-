e357f9204d79f1fd677ca225bf8aee704cee2b4e
luis jesus villegas castillo <lc77278@gmail.com>
2023-01-03 21:08:14 -0600

    query and mutation graphql

---
+++ src/index.js
@@ -0,0 +1,65 @@
+const { ApolloServer, gql } = require('apollo-server')
+const persons = require('./mocks/persons.js')
+const { v4: uuid } = require('uuid')
+
+const typeDefs = gql`
+  type Address {
+    street: String!
+    city: String!
+  }
+
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person!]!
+    findPerson(name: String!): Person
+  }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+  }
+
+`
+
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (_, args) =>
+      persons.find(p => p.name === args.name)
+  },
+  Person: {
+    address: (root) => ({
+      street: root.street,
+      city: root.city
+    })
+  },
+  Mutation: {
+    addPerson: (_, args) => {
+      const person = { ...args, id: uuid() }
+      persons.push(person)
+      return person
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen()
+  .then(({ url }) => {
+    console.log(`Server ready at ${url}`)
+  })
